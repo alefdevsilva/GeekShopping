b8832d9bf6b28a654ee1ec23049b5b94bbd0ba71
AlefSilva <alef.dev.silva@gmail.com>
2024-04-15 22:33:14 -0300

    - Definindo as propriedades da interface IProductRepository

--- GeekShopping.ProductAPI/Model/Repository/IProductRepository.cs
+++ GeekShopping.ProductAPI/Model/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace GeekShopping.ProductAPI.Model.Repository
+﻿using GeekShopping.ProductAPI.Data.DTOs;
+
+namespace GeekShopping.ProductAPI.Model.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDTO>> FindAll();
+        Task<ProductDTO> FindById(long id);
+        Task<ProductDTO> Create(ProductDTO productDTO);
+        Task<ProductDTO> Update(ProductDTO productDTO);
+        Task<bool> Delete(long id);
     }
 }
