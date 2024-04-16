526c6e068d5ae24380f4f14e9f2d06ad6e8ce737
AlefSilva <alef.dev.silva@gmail.com>
2024-04-16 08:15:46 -0300

    - Definindo as operações da interface IProductService

---
+++ GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductViewModel>> FindAllProducts();
+        Task<ProductViewModel> FindProductById(long id);
+        Task<ProductViewModel> CreateProduct(ProductViewModel productViewModel);
+        Task<ProductViewModel> UpdateProduct(ProductViewModel productViewModel);
+        Task<bool> DeleteProductById(long id);
+    }
+}
