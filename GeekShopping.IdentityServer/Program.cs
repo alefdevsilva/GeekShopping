3739820901f4cc838cff838e1b457601e23f69e0
AlefSilva <alef.dev.silva@gmail.com>
2024-04-23 02:18:32 -0300

    - Criando o IdentityServer com o Framework Duende; - Add pacotes do Nuget ao Identity Server; - Add DB Context ao IdentityServer; - Implementando a classe IdentityConfiguration; - Configurando Clients no IdentityServer; - Add Configurações de Banco de dados e segurança; - Habilitando HTTPS e setando portas; - Criando base de dados e executando Migrations

---
+++ GeekShopping.IdentityServer/Program.cs
@@ -0,0 +1,54 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration["SQLServerConnection:SQLServerConnectionString"];
+builder.Services.AddDbContext<SQLServerContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<SQLServerContext>()
+    .AddDefaultTokenProviders();
+
+var builders = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+builders.AddDeveloperSigningCredential();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
