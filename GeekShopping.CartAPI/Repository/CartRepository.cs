6e0829356722c175f9b66423a56ae87dc3a2b855
AlefSilva <alef.dev.silva@gmail.com>
2024-05-09 20:16:17 -0300

    - Corrigindo classe de contexto; - Implementando método SaveOrUpdate

--- GeekShopping.CartAPI/Repository/CartRepository.cs
+++ GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using GeekShopping.CartAPI.Model;
 using GeekShopping.CartAPI.Model.Context;
 using GeekShopping.Data.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeekShopping.CartAPI.Repository
 {
@@ -15,34 +17,80 @@ namespace GeekShopping.CartAPI.Repository
             _mapper = mapper;
         }
 
-        public Task<bool> ApplyCoupon(string userId, string couponCode)
+        public async Task<bool> ApplyCoupon(string userId, string couponCode)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> ClearCart(string userId)
+        public async Task<bool> ClearCart(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<CartViewModel> FindCartByUserId(string userId)
+        public async Task<CartViewModel> FindCartByUserId(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> RemoveCoupon(string userId)
+        public async Task<bool> RemoveCoupon(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> RemoveFromcart(long cartDetailsId)
+        public async Task<bool> RemoveFromcart(long cartDetailsId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<CartViewModel> SaveOrUpdateCart(CartViewModel cartViewModel)
+        public async Task<CartViewModel> SaveOrUpdateCart(CartViewModel cartViewModel)
         {
-            throw new NotImplementedException();
+            var cart = _mapper.Map<Cart>(cartViewModel);
+
+            var product = await _context.Products.FirstOrDefaultAsync(
+                p => p.Id == cartViewModel.CartDetails.FirstOrDefault().ProductId);
+
+            if (product == null)
+            {
+                _context.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+                await _context.SaveChangesAsync();
+            }
+
+            var cartHeader = await _context.CartHeaders.AsNoTracking().FirstOrDefaultAsync(
+                c => c.UserId == cart.CartHeader.UserId);
+
+            if (cartHeader == null)
+            {
+                _context.CartHeaders.Add(cart.CartHeader);
+                await _context.SaveChangesAsync();
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.Id;
+                cart.CartDetails.FirstOrDefault().Product = null;
+                _context.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var cartDetail = await _context.CartDetails.AsNoTracking().FirstOrDefaultAsync(
+                    p => p.ProductId == cartViewModel.CartDetails.FirstOrDefault().ProductId &&
+                    p.CartHeaderId == cartHeader.Id);
+
+                if (cartDetail == null)
+                {
+                    cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.Id;
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    _context.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    cart.CartDetails.FirstOrDefault().Count += cartDetail.Count;
+                    cart.CartDetails.FirstOrDefault().Id = cartDetail.Id;
+                    cart.CartDetails.FirstOrDefault().CartHeaderId = cartDetail.CartHeaderId;
+                    _context.CartDetails.Update(cart.CartDetails.FirstOrDefault());
+                    await _context.SaveChangesAsync();
+                }
+            }
+            return _mapper.Map<CartViewModel>(cart);
         }
     }
 }
