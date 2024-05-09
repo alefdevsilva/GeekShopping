1d0aac0ce6aa63bb0196f9c959e0d764a3eac8ff
AlefSilva <alef.dev.silva@gmail.com>
2024-05-09 19:47:07 -0300

    - Criando interface e classe de repositório de carinho de compras

---
+++ GeekShopping.CartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.Data.ViewModels;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartViewModel> FindCartByUserId(string userId);
+        Task<CartViewModel> SaveOrUpdateCart(CartViewModel cartViewModel);
+        Task<bool> RemoveFromcart(long cartDetailsId);
+        Task<bool> ApplyCoupon(string userId, string couponCode);
+        Task<bool> RemoveCoupon(string userId);
+        Task<bool> ClearCart(string userId);
+    }
+}
