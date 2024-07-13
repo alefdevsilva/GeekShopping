40a2aa62b2a16c62bc112cc5819963256257d8e8
AlefSilva <alef.dev.silva@gmail.com>
2024-07-12 22:07:01 -0300

    - Criando ViewModels representnado a mensagem enviada ao RabbitMQ

---
+++ GeekShopping.OrderAPI/Messages/UpdatePaymentResultViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShopping.OrderAPI.Messages
+{
+    public class UpdatePaymentResultViewModel
+    {
+        public long OrderId { get; set; }
+        public bool Status{ get; set; }
+        public string Email { get; set; }
+    }
+}
