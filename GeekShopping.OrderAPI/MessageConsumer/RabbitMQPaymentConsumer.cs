343fcc1feb8137c12a497137aa024f31f2236c1f
AlefSilva <alef.dev.silva@gmail.com>
2024-07-22 22:20:33 -0300

    - Consumindo a mensagem de resposta no RabbitMQ

---
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
@@ -0,0 +1,64 @@
+﻿
+using GeekShopping.OrderAPI.Messages;
+using GeekShopping.OrderAPI.Repository;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.OrderAPI.MessageConsumer
+{
+    public class RabbitMQPaymentConsumer : BackgroundService
+    {
+        private readonly OrderRepository _orderRepository;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQPaymentConsumer(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+            var factory = new ConnectionFactory
+            {
+                HostName = "host.docker.internal",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672
+
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "orderpaymentresultqueue", false, false, false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (channel, evt) =>
+            {
+                var content = Encoding.UTF8.GetString(evt.Body.ToArray());
+                var checkoutHeaderViewModel = JsonSerializer.Deserialize<UpdatePaymentResultViewModel>(content);
+                UpdatePaymentStatus(checkoutHeaderViewModel).GetAwaiter().GetResult();
+                _channel.BasicAck(evt.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume("orderpaymentresultqueue", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task UpdatePaymentStatus(UpdatePaymentResultViewModel updatePaymentResultViewModel)
+        {
+            try
+            {
+                await _orderRepository.UpdateOrderPaymentStatus(
+                    updatePaymentResultViewModel.OrderId,
+                    updatePaymentResultViewModel.Status);
+            }
+            catch (Exception)
+            {
+                //Log
+                throw;
+            }
+        }
+    }
+}
