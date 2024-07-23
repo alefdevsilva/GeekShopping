7e9aa7c4dfcbfefece1724e8fd2542924d36c607
AlefSilva <alef.dev.silva@gmail.com>
2024-07-22 21:38:43 -0300

    - Implementando classe responsável por enviar mensagem ao RabbiMQ

---
+++ GeekShopping.PaymentAPI/RabbitMQSender/RabbitMQMessageSender.cs
@@ -0,0 +1,55 @@
+﻿using GeekShopping.MessageBus;
+using GeekShopping.PaymentAPI.Messages;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.PaymentAPI.RabbitMQSender
+{
+    public class RabbitMQMessageSender : IRabbitMQMessageSender
+    {
+        private readonly string _hostName;
+        private readonly int _port;
+        private readonly string _password;
+        private readonly string _userName;
+        private IConnection _connection;
+
+        public RabbitMQMessageSender()
+        {
+            _hostName = "host.docker.internal";
+            _port = 5672;
+            _password = "guest";
+            _userName = "guest";
+        }
+
+        public void SendMessage(BaseMessage messsage, string queueName)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostName,
+                Port = _port,
+                UserName = _userName,
+                Password = _password,
+            };
+
+            _connection = factory.CreateConnection();
+
+            using var channel = _connection.CreateModel();
+            channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
+            byte[] body = GetMessageAsByteArray(messsage);
+            channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+        }
+
+        private byte[] GetMessageAsByteArray(BaseMessage messsage)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            var json = JsonSerializer.Serialize<UpdatePaymentResultMessage>((UpdatePaymentResultMessage)messsage, options);
+            var body = Encoding.UTF8.GetBytes(json);
+            return body;
+        }
+    }
+}
